dc1891e5bf15e1fbc5da96542b5cc3f21ea18c4d
Christian Hensman <n11247321@qut.edu.au>
2022-10-01 10:44:57 +1000

    Added css, js and index over. Need to modify index to work with MVC

---
+++ website/static/js/EventRating.js
@@ -0,0 +1,22 @@
+//On input value change event
+function rating() {
+    var inputs = document.getElementsByClassName("ratingRadio");
+    var labels = document.getElementsByTagName("label");
+    var radioValue = 0;
+
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].checked) {
+            break;
+        }
+        radioValue++;
+    }
+
+    for (var i = 0; i < labels.length; i++) {
+        if (radioValue >= 0) {
+            labels[i].style.color = "orange";
+            radioValue--;
+        } else {
+            labels[i].style.color = "#212529";
+        }
+    }
+}
