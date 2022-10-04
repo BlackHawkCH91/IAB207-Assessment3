ace9c78debfa3f3513eb203beccb52cc6d75e7b6
Christian Hensman <n11247321@qut.edu.au>
2022-10-05 01:38:26 +1000

    Included all html files. Added basic functionality such as loops, if statements, etc.

--- website/views.py
+++ website/views.py
@@ -10,3 +10,11 @@ def index():
 @bp.route('/UserBookingHistory')
 def userBookingHistory():
     return render_template("userBookingHistory.html")
+
+@bp.route('/EventCreation')
+def eventCreation():
+    return render_template("eventCreation.html")
+
+@bp.route('/Event')
+def event():
+    return render_template("event.html")
