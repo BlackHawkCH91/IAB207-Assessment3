fe320c3d309ecde8c7092231ccdda14107a18462
Harrison <103091118+HarrisonWyatt@users.noreply.github.com>
2022-10-15 14:19:45 +1000

    merging bug

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,5 @@
 #import flask - from the package import class
-from flask import Flask 
+from flask import Flask, render_template
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
@@ -9,11 +9,12 @@ db=SQLAlchemy()
 #initialize the login manager
 login_manager = LoginManager()
 
+app=Flask(__name__)  # this is the name of the module/package that is calling this app
+
 #create a function that creates a web application
 # a web server will run this web application
 def create_app():
   
-    app=Flask(__name__)  # this is the name of the module/package that is calling this app
     app.debug=True
     app.secret_key='utroutoru'
     #set the app configuration data 
@@ -27,20 +28,32 @@ def create_app():
     
     #set the name of the login function that lets user login
     # in our case it is auth.login (blueprintname.viewfunction name)
+    # Intitialise Login Manager
+    login_manager = LoginManager()
     login_manager.login_view='auth.login'
     login_manager.init_app(app)
 
-    #create a user loader function takes userid and returns User
-    #from .models import User  # importing here to avoid circular references
-    #@login_manager.user_loader
-    #def load_user(user_id):
-    #    return User.query.get(int(user_id))
+    # Create loader function that takes a user ID and returns a User
+    from .models import Users # importing here to avoid circular references
+    @login_manager.user_loader
+    def load_user(user_id):
+        return Users.query.get(int(user_id))
+
+    # Handle 404 error
+    @app.errorhandler(404) 
+    # inbuilt function which takes error as parameter 
+    def not_found(e): 
+        return render_template("404.html")
+
 
     #importing views module here to avoid circular references
     # a commonly used practice.
     from . import views
     app.register_blueprint(views.bp)
 
+    from . import events
+    app.register_blueprint(events.bp)
+    
     from . import auth
     app.register_blueprint(auth.bp)
     
